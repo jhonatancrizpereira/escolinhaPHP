1af11d750096ed924d3e08f593989e1ae810f79f
jhonatancrizpereira <jhonatancrizpereira@gmail.com>
2015-11-18 20:39:45 -0200

    Criação do arquivo index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Manuseio de Formulario com PHP</h1>
+        <h2>Referências:</h2>
+        <ul>
+            <li><a href="http://www.w3schools.com/php/php_forms.asp">W3Schools</a></li>
+            <li><a href="http://php.net/">Manual do PHP</a></li>
+        </ul>
+        <?php
+           
+        ?>
+    </body>
+</html>
