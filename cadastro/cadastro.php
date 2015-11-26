3e9fe71b91e0a845fba7411d90007e78b00b3f35
jhonatancrizpereira <jhonatancrizpereira@gmail.com>
2015-11-26 03:38:42 -0200

    Confirmação de e-mail pronta (sem o envio do email) CRUD completo - inserção, exclusão, listagem e atualização.

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -89,9 +89,15 @@ if (isset($_POST['btn'])) {
             //Link de exclusão
             echo "<a href='cadastro.php?cod=d&hash=$r[cod]' title='Clique para excluir'>";
             echo $r['cod'];
-            echo "</a>";
+            echo "</a>" . "\t";
             
-            echo $r['situacao'] . "\t";
+            //Link para ser enviado por email
+            $link = "<a href='".$_SERVER['PHP_SELF']."?cod=e&hash=$r[cod]' title='Clique aqui para confirmar o e-mail'>";
+            $link .=$r['situacao'] . "\t";
+            $link .= "</a>";
+            
+            echo $link;
+            
             echo converteDataMySQLPHP($r['dtCadastro']) . "\t";
             echo converteDataMySQLPHP($r['dtAtualizacao']);
             echo "</p>\n";
@@ -101,6 +107,20 @@ if (isset($_POST['btn'])) {
     //Exclusão de um registro
     elseif($_GET['cod'] == 'd' && isset ($_GET['hash'])){
         $sql = "delete from lista where cod = :hash";
+        $hash = filter_input(INPUT_GET, 'hash', FILTER_SANITIZE_STRING);
+        
+        //echo "<h1>$hash</h1>";
+        
+        $p = $conn->prepare($sql);
+        $q = $p->execute(array(':hash'=>$hash));
+        
+        header("Location: cadastro.php?cod=listar");
+    }
+    
+    //Atualização da situação cadastral
+    //Confirmação de e-mail
+    elseif($_GET['cod'] == 'e' && isset ($_GET['hash'])){
+        $sql = "update lista set situacao=1, dtAtualizacao= now() where cod = :hash";
         $hash = filter_input(INPUT_GET, 'hash', FILTER_SANITIZE_STRING);
         
         //echo "<h1>$hash</h1>";
